8c6f91462b3989c9b06d73597d86f7f8574c86d5
prachisahu <prachisah@gmail.com>
2023-02-27 11:22:29 +0000

    Added logs

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -18,18 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
-       // registry.setApplicationDestinationPrefixes("/room");
+        registry.enableSimpleBroker("/topic");
 
-
-        //   Use this for enabling a Full featured broker like RabbitMQ
-
-        /*
-        registry.enableStompBrokerRelay("/topic")
-                .setRelayHost("localhost")
-                .setRelayPort(61613)
-                .setClientLogin("guest")
-                .setClientPasscode("guest");
-        */
     }
 }
--- src/main/java/com/example/websocketdemo/controller/ChatController.java
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -16,27 +16,46 @@ import java.util.Date;
 @Controller
 public class ChatController {
 
+    public final static String TOPIC_URL = "/topic/public/";
     private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
-
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
-
+    /**
+     * This method is used to send the typing notification all the user logged in
+     *
+     * @param chatMessage
+     * @return
+     */
     @MessageMapping("/chat.type")
     public ChatMessage sendTypeStatus(@Payload ChatMessage chatMessage) {
-        simpMessagingTemplate.convertAndSend("/topic/public/" + chatMessage.getRoom(), chatMessage);
+        simpMessagingTemplate.convertAndSend(TOPIC_URL + chatMessage.getRoom(), chatMessage);
         return chatMessage;
     }
+
+    /**
+     * This method is used to send the message to the room logged in
+     *
+     * @param chatMessage
+     * @return
+     */
 
     @MessageMapping("/chat.sendMessage")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         String time = new SimpleDateFormat("HH:mm").format(new Date());
         chatMessage.setTime(time);
-        logger.info("Message sent by " + chatMessage.getSender()  + " from  room " + chatMessage.getRoom() +  " : message  "+ chatMessage.getContent() );
-        simpMessagingTemplate.convertAndSend("/topic/public/" + chatMessage.getRoom(), chatMessage);
+        logger.info("Message sent by " + chatMessage.getSender() + " from  room " + chatMessage.getRoom() + " : message  " + chatMessage.getContent());
+        simpMessagingTemplate.convertAndSend(TOPIC_URL + chatMessage.getRoom(), chatMessage);
         return chatMessage;
     }
 
+    /**
+     * This method is used to add the users to the room.
+     *
+     * @param chatMessage
+     * @param headerAccessor
+     * @return
+     */
 
     @MessageMapping("/chat.addUser")
     public ChatMessage addUser(@Payload ChatMessage chatMessage,
@@ -45,9 +64,10 @@ public class ChatController {
         String time = new SimpleDateFormat("HH:mm").format(new Date());
         chatMessage.setTime(time);
         chatMessage.getUserList().add(chatMessage.getSender());
-        simpMessagingTemplate.convertAndSend("/topic/public/" + chatMessage.getRoom(), chatMessage);
+        simpMessagingTemplate.convertAndSend(TOPIC_URL + chatMessage.getRoom(), chatMessage);
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         headerAccessor.getSessionAttributes().put("room", chatMessage.getRoom());
+
 
         return chatMessage;
     }
