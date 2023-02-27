7ddd5ddb0b5d1f73c6249deb8561e79f2905ab88
prachisahu <prachisah@gmail.com>
2023-02-27 09:10:48 +0000

    Added logs

---
+++ Readme.md
@@ -0,0 +1,33 @@
+## Spring Boot WebSocket Chat Appplication
+
+
+
+## Requirements
+
+1. Java - 11
+
+2. Maven - 3.x.x
+
+## Steps to Setup
+
+**1. Clone the application**
+
+```bash
+git clone https://github.com/prachisahu25/websocket-chat.git
+
+**2. Build and run the app using maven**
+
+```bash
+
+cd websocket-chat
+mvn package
+java -jar target/websocket-demo-0.0.1-SNAPSHOT.jar
+```
+
+Alternatively, you can run the app directly without packaging it like so -
+
+```bash
+mvn spring-boot:run
+```
+
+
--- src/main/java/com/example/websocketdemo/controller/ChatController.java
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -1,41 +1,38 @@
 package com.example.websocketdemo.controller;
 
 import com.example.websocketdemo.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageTypeMessageCondition;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import javax.websocket.OnMessage;
-import javax.websocket.Session;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Controller
 public class ChatController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
 
     @MessageMapping("/chat.type")
-//    @SendTo("/topic/public")
     public ChatMessage sendTypeStatus(@Payload ChatMessage chatMessage) {
-
-        simpMessagingTemplate.convertAndSend("/topic/public/"+ chatMessage.getRoom(), chatMessage);
+        simpMessagingTemplate.convertAndSend("/topic/public/" + chatMessage.getRoom(), chatMessage);
         return chatMessage;
     }
 
     @MessageMapping("/chat.sendMessage")
-//    @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         String time = new SimpleDateFormat("HH:mm").format(new Date());
         chatMessage.setTime(time);
+        logger.info("Message sent by " + chatMessage.getSender()  + " from  room " + chatMessage.getRoom() +  " : message  "+ chatMessage.getContent() );
         simpMessagingTemplate.convertAndSend("/topic/public/" + chatMessage.getRoom(), chatMessage);
         return chatMessage;
     }
@@ -48,9 +45,9 @@ public class ChatController {
         String time = new SimpleDateFormat("HH:mm").format(new Date());
         chatMessage.setTime(time);
         chatMessage.getUserList().add(chatMessage.getSender());
-        simpMessagingTemplate.convertAndSend("/topic/public/"+ chatMessage.getRoom(), chatMessage);
+        simpMessagingTemplate.convertAndSend("/topic/public/" + chatMessage.getRoom(), chatMessage);
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        headerAccessor.getSessionAttributes().put("room",chatMessage.getRoom());
+        headerAccessor.getSessionAttributes().put("room", chatMessage.getRoom());
 
         return chatMessage;
     }
